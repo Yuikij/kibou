adbfc015a9ab8afe02dcfd7ae1ed252fcff73e52
zxj <775718540@qq.com>
2023-10-14 00:04:21 +0800

    init

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-    title: 'My Site',
+    title: 'Yuikij\'s  blog',
     tagline: 'Dinosaurs are cool',
     favicon: 'img/favicon.ico',
 
@@ -52,17 +52,9 @@ const config = {
             ({
                 docs: {
                     sidebarPath: require.resolve('./sidebars.js'),
-                    // Please change this to your repo.
-                    // Remove this to remove the "edit this page" links.
-                    editUrl:
-                        'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
                 },
                 blog: {
                     showReadingTime: true,
-                    // Please change this to your repo.
-                    // Remove this to remove the "edit this page" links.
-                    editUrl:
-                        'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
                 },
                 theme: {
                     customCss: require.resolve('./src/css/custom.css'),
@@ -84,17 +76,42 @@ const config = {
                 },
                 items: [
                     {
-                        type: 'docSidebar',
-                        sidebarId: 'tutorialSidebar',
+                        type: 'dropdown',
+                        label: '基础知识',
+                        sidebarId: 'basicKnowledge',
+                        to: '/docs/basicKnowledge',
                         position: 'left',
-                        label: 'Tutorial',
+                        items: [
+                            {
+                                type: 'docSidebar',
+                                sidebarId: 'programmingLanguage',
+                                label: '编程语言',
+                            },
+                            {
+                                type: 'docSidebar',
+                                sidebarId: 'network',
+                                label: '网络知识',
+                            },
+                            // ... more items
+                        ],
                     },
                     {
-                        type: 'search',
-                        position: 'right',
+                        type: 'docSidebar',
+                        sidebarId: 'database',
+                        position: 'left',
+                        label: '数据库',
                     },
-                    {to: '/blog', label: 'Blog', position: 'left'},
+                    {
+                        type: 'docSidebar',
+                        sidebarId: 'distributedSystems',
+                        position: 'left',
+                        label: '分布式系统',
+                    },
 
+
+
+                    // right
+                    {to: '/blog', label: 'Blog', position: 'right'},
                     {
                         type: 'dropdown',
                         label: '备忘录',
@@ -109,18 +126,8 @@ const config = {
                         ],
                     },
                     {
-                        type: 'dropdown',
-                        label: 'Community',
-                        position: 'left',
-                        items: [
-                            {
-                                type: 'docSidebar',
-                                label: 'Social',
-                                sidebarId: 'tutorialSidebar',
-                            },
-                            {to: '/blog', label: 'Blog'}
-                            // ... more items
-                        ],
+                        type: 'search',
+                        position: 'right',
                     },
                     {
                         href: 'https://github.com/facebook/docusaurus',
