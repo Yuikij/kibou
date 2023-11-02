8b3ecdda4f9fa3a6be001843c92f706942b61eea
zxj <775718540@qq.com>
2023-11-02 15:03:09 +0800

    外语阅读组件

--- src/components/Language/JapaneseText.js
+++ src/components/Language/JapaneseText.js
@@ -1,49 +1,65 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {columnCenterStyle} from "../Css";
 
 
-function JapaneseText({text, annotations}) {
+function JapaneseText({text, annotations, translations}) {
+
+    const renderTranslationsText = (text, translations) => {
+        const parts = text.split(/(\$trans{.*?:\[.*?]}trans)/s);
+        return parts.map((part, index) => {
+            let translation = "fix";
+            if (part.startsWith("$trans{") && part.endsWith("]}trans")) {
+                const key = part.slice(7, -7);
+                let [word, translationKey] = key.split(":[");
+                translation = translations[translationKey];
+                part = word;
+            }
+            return (<div className={"annotated-word"} key={index}>
+                    {translation === "fix" ? <dev key={index}>
+                            {renderAnnotatedText(part, annotations)}
+                        </dev> :
+                        <div>
+                            <div className={"translation-word"} style={{display: "block"}}>
+                                {translation}
+                            </div>
+                            {renderAnnotatedText(part, annotations)}
+                        </div>
+                    }
+                </div>
+            );
+        });
+    }
+
     const renderAnnotatedText = (text, annotations) => {
-        const parts = text.split(/(\${.*?:.*?})/);
+        const parts = text.split(/(\$anno{.*?:.*?})/);
         return parts.map((part, index) => {
             let annotation = "fix";
-            if (part.startsWith("${") && part.endsWith("}")) {
-                const key = part.slice(2, -1);
+            if (part.startsWith("$anno{") && part.endsWith("}")) {
+                const key = part.slice(6, -1);
                 let [word, annotationKey] = key.split(":");
                 annotation = annotations[annotationKey];
                 part = word;
             }
-            // if (annotation === "fix") {
-            //    return  (part.split(" ").map((partWord, index) =>
-            //         (<div style={{display: "inline-block"}}>
-            //             <div style={columnCenterStyle}>
-            //                 {<div className="annotation" style={annotation === "fix" ? {visibility: "hidden"} : {}}>
-            //                     {annotation}
-            //                 </div>}
-            //                 <span key={index} className="annotated-word">
-            //                      {partWord}
-            //             </span>
-            //             </div>
-            //         </div>)
-            //     ))
-            // }
 
-            return (<div style={{display: "inline-block"}}>
-                    <div style={columnCenterStyle}>
-                        {<div className="annotation" style={annotation === "fix" ? {visibility: "hidden"} : {}}>
-                            {annotation}
-                        </div>}
-                        <span key={index} className="annotated-word">
+            return (<div className={"annotated-word"} key={index}>
+                    {annotation === "fix" ? <span key={index}>
                                  {part}
-                        </span>
-                    </div>
+                        </span> :
+                        <ruby>
+                            {part}
+                            <rt>
+                                {/*<div style={{position:"absolute"}}>*/}
+                                {annotation}
+                                {/*</div>*/}
+                            </rt>
+                        </ruby>
+                    }
                 </div>
             );
         });
     };
-
     return (<div>
-        {renderAnnotatedText(text, annotations)}
+        {renderTranslationsText(text, translations)}
     </div>);
 }
 
