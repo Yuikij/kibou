984f043df227c789c69d8cbcd6662a984114c885
Yuikij <775718540@qq.com>
2024-08-14 00:41:55 +0800

    docs(naturalLanguage): 添加明日晴朗的注释和翻译
    - 引入JapaneseText2组件以支持多行日语注释和翻译。
    - 明日晴朗的疑问现在有了详细的日语注释和中文翻译。
    - 修正了现有注释的格式，使其与新的组件兼容。
    
    docs(Linux): 添加磁盘操作指南
    
    - 提供了查看磁盘分区信息的指南，包括使用fdisk和lsblk命令。
    - 以markdown格式添加了Linux基本软件文档。
    
    docs(cs186): 更新proj3.mdx，添加Hash Join和Sort-Merge Join的说明
    
    - 在cs186项目3文档中添加了Hash Join和Sort-Merge Join部分的详细说明。
    - 确保了作业说明的完整性，便于学生理解和执行。
    
    docs(publishClass): 更新cs186项目3文档，添加连接类型和评估标准
    
    - 在cs186项目3文档中添加了连接类型和评估标准部分。
    - 为学生提供了明确的项目目标和评估指标，以便更好地准备作业。

---
+++ docs/basicSoftware/Operation/Docker.md
@@ -0,0 +1,34 @@
+## 常用命令
+### 在执行的情况下进入容器
+* 交互式启动容器
+```shell
+docker run -it my_image /bin/bash
+```
+* 使用 --entrypoint , 覆盖容器的默认入口点
+```shell
+docker run -it --entrypoint /bin/bash my_image
+```
+
+## 运维
+### 修改存储的路径
+1. 修改Docker配置文件
+Docker的默认存储位置通常是在 `/var/lib/docker`。要更改存储位置，你需要修改Docker的配置文件。
+在 `/etc/docker/` 目录下，如果没有 `daemon.json` 文件，可以创建一个。然后编辑这个文件来指定新的存储位置：
+```bash
+sudo nano /etc/docker/daemon.json
+```
+添加或修改如下内容：
+```json
+{
+  "data-root": "/new/docker/path"
+}
+```
+2. 移动现有的数据（可选）
+
+如果你希望保留现有的数据，你需要将原有的Docker数据目录移动到新位置：
+
+```bash
+sudo rsync -aP /var/lib/docker/ /new/docker/path/
+```
+3. 重启Docker服务
+
---
+++ src/components/Language/JapaneseText2.js
@@ -0,0 +1,71 @@
+import React from 'react';
+
+// 传递三个数组：文本、翻译和注释
+function JapaneseText2({ texts, translations, annotations }) {
+
+    // 渲染带注释的文本
+    const renderTextWithAnnotations = (text, annotation) => {
+        let result = [];
+        let currentIndex = 0;
+
+        // 遍历每个词汇和其注释
+        Object.keys(annotation).forEach((word) => {
+            // 查找词汇在文本中的位置
+            let index = text.indexOf(word, currentIndex);
+            if (index !== -1) {
+                // 将词汇之前的部分加入结果
+                if (index > currentIndex) {
+                    result.push(
+                        <span key={index}  style={{ whiteSpace: 'pre-line' }}>
+                            {text.substring(currentIndex, index)}
+                        </span>
+                    );
+                }
+
+                // 添加带注释的词汇
+                result.push(
+                    <div className={"annotated-word"}>
+                        <ruby key={index}>
+                            {word}
+                            <rt>{annotation[word]}</rt>
+                        </ruby>
+                    </div>
+                );
+
+                // 更新当前索引
+                currentIndex = index + word.length;
+            }
+        });
+
+        // 将文本剩余部分加入结果
+        if (currentIndex < text.length) {
+            result.push(text.substring(currentIndex));
+        }
+
+        return <div className={"annotated-word"} >{result}</div>;
+    };
+
+    // 渲染每行内容
+    const renderContent = () => {
+        return texts.map((text, index) => {
+            const translation = translations[index] || "";
+            const annotation = annotations[index] || {};
+
+            return (
+                <div key={index} className="annotated-word">
+                    {translation && (
+                        <div className={"translation-word"} style={{display: "block"}}>
+                            {translation}
+                        </div>
+                    )}
+                        {renderTextWithAnnotations(text, annotation)}
+                    
+                </div>
+            );
+        });
+    };
+
+    return <div>{renderContent()}</div>;
+}
+
+export default JapaneseText2;
